5b3a8b51d580855504488dd36eb81ab90ee6a259
he SUN <2968655s@student.gla.ac.uk>
2025-03-02 01:11:45 +0000

    Aa

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,21 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+    },
+  },
   plugins: [react()],
   server: {
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://ems-21j1.onrender.com',
         changeOrigin: true,
+        secure: false,  // 如果是 HTTPS，请启用
       },
     },
   },
+  
 })
